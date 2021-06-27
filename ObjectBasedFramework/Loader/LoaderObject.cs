c18200d3624fd95bf19397b58b7b1a67687d29b2
CSX8600 <cnwaj@hotmail.com>
2021-06-26 22:12:20 -0500

    Added basic system record loading

---
+++ ObjectBasedFramework/Loader/LoaderObject.cs
@@ -0,0 +1,92 @@
+﻿using ClussPro.ObjectBasedFramework.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClussPro.ObjectBasedFramework.Loader
+{
+    public class LoaderObject
+    {
+        public Type DataObjectType { get; set; }
+
+        public LoaderObject(Type dataObjectType)
+        {
+            DataObjectType = dataObjectType;
+        }
+
+        public Guid SystemID { get; set; }
+        public Dictionary<string, object> Values { get; set; }
+
+        internal byte[] CalculateHash()
+        {
+            Dictionary<string, object> setValues = GetValuesByField();
+
+            SchemaObject thisObject = Schema.Schema.GetSchemaObject(DataObjectType);
+            StringBuilder hashStringBuilder = new StringBuilder();
+            foreach (Field field in thisObject.GetFields().Where(f => f.IsSystemLoaded))
+            {
+                hashStringBuilder.Append($"{field.FieldName}=");
+
+                if (setValues.ContainsKey(field.FieldName))
+                {
+                    hashStringBuilder.Append(setValues[field.FieldName]);
+                }
+
+                hashStringBuilder.Append(";");
+            }
+
+            string hashString = hashStringBuilder.ToString();
+
+            using (MD5 md5 = MD5.Create())
+            {
+                return md5.ComputeHash(Encoding.UTF8.GetBytes(hashString));
+            }
+        }
+
+        public virtual Dictionary<string, object> GetValuesByField()
+        {
+            return Values;
+        }
+    }
+
+    public class LoaderObject<T> : LoaderObject where T:DataObject, ISystemLoaded
+    {
+        public Expression<Func<T, Dictionary<object, object>>> DataObjectValues { get; set; }
+
+        public LoaderObject(Guid systemID) : base(typeof(T))
+        {
+            SystemID = systemID;
+        }
+
+        public LoaderObject(string systemID) : this(new Guid(systemID)) { }
+
+        public override Dictionary<string, object> GetValuesByField()
+        {
+            Dictionary<string, object> returnValue = new Dictionary<string, object>();
+
+            if (!(DataObjectValues.Body is ListInitExpression listInitExpression))
+            {
+                return returnValue;
+            }
+
+            foreach(ElementInit elementInit in listInitExpression.Initializers.OfType<ElementInit>())
+            {
+                if (elementInit.Arguments.Count != 2 ||
+                    !(elementInit.Arguments[0] is MemberExpression memberExpression) ||
+                    !(elementInit.Arguments[1] is ConstantExpression constantExpression) ||
+                    !(memberExpression.Member is PropertyInfo propertyInfo))
+                {
+                    continue;
+                }
+
+                returnValue[propertyInfo.Name] = constantExpression.Value;
+            }
+
+            return returnValue;
+        }
+    }
+}
--- WebModels/security/Permission.cs
+++ WebModels/security/Permission.cs
@@ -1,12 +1,14 @@
 ﻿using ClussPro.ObjectBasedFramework;
+using ClussPro.ObjectBasedFramework.Schema;
 using ClussPro.ObjectBasedFramework.Schema.Attributes;
 using ClussPro.ObjectBasedFramework.Validation.Attributes;
+using System;
 using System.Collections.Generic;
 
 namespace WebModels.security
 {
     [Table("D2956313-9B89-4093-8DB6-0F5EC9C42088")]
-    public class Permission : DataObject
+    public class Permission : DataObject, ISystemLoaded
     {
         protected Permission() : base() { }
 
@@ -18,7 +20,7 @@ namespace WebModels.security
         }
 
         private string _name;
-        [Field("6B926A96-7522-4410-9C0A-713982DF66B1", DataSize = 100)]
+        [Field("6B926A96-7522-4410-9C0A-713982DF66B1", DataSize = 100, IsSystemLoaded = true)]
         public string Name
         {
             get { CheckGet(); return _name; }
@@ -26,12 +28,28 @@ namespace WebModels.security
         }
 
         private string _key;
-        [Field("26BE4F84-CC5E-45FD-859D-6B999BD896FF", DataSize = 50)]
+        [Field("26BE4F84-CC5E-45FD-859D-6B999BD896FF", DataSize = 50, IsSystemLoaded = true)]
         [Required]
         public string Key
         {
             get { CheckGet(); return _key; }
             set { CheckSet(); _key = value; }
+        }
+
+        private Guid? _systemID;
+        [Field("6428F736-81F6-4BD9-8C27-92DFAE7891E7")]
+        public Guid? SystemID
+        {
+            get { CheckGet(); return _systemID; }
+            set { CheckSet(); _systemID = value; }
+        }
+
+        private byte[] _systemHash;
+        [Field("DA8D8928-FAC5-4F4B-96F0-B239E8B7135A")]
+        public byte[] SystemHash
+        {
+            get { CheckGet(); return _systemHash; }
+            set { CheckSet(); _systemHash = value; }
         }
 
         #region Relationships

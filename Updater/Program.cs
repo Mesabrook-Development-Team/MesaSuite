4ba5fcee176f986383f255378c610f6b39bd35a4
CSX8600 <cnwaj@hotmail.com>
2022-01-17 12:48:17 -0600

    Make folder browser an embeddedresource

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Updater
 {
@@ -16,8 +18,24 @@ namespace Updater
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             StartupArguments.SetupArguments(args);
             Application.Run(new frmPrompts());
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (!string.Equals(args.Name, "BetterFolderBrowser, Version=1.2.0.0, Culture=neutral, PublicKeyToken=null"))
+            {
+                return null;
+            }
+
+            using(Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Updater.BetterFolderBrowser.dll"))
+            {
+                byte[] assemblyData = new byte[stream.Length];
+                stream.Read(assemblyData, 0, (int)stream.Length);
+                return Assembly.Load(assemblyData);
+            }
         }
     }
 }

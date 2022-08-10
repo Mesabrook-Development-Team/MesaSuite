e57803456ce126963ea797ca06d07a418478debf
Adam Clussman <adamc@skyward.com>
2022-08-10 15:16:19 -0500

    Actually send outbound emails

---
+++ MesaService/ServiceTasks/SendOutboundEmails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using ClussPro.ObjectBasedFramework.DataSearch;
+using WebModels.mesasys;
+
+namespace MesaService.ServiceTasks
+{
+    internal class SendOutboundEmails : IServiceTask
+    {
+        public string Name => "Send Outbound Emails";
+
+        private DateTime _nextRunTime = DateTime.Now;
+        public DateTime NextRunTime => _nextRunTime;
+
+        public bool Run()
+        {
+            string server = ConfigurationManager.AppSettings.GetValues("SMTPServer")?.FirstOrDefault();
+            string strPort = ConfigurationManager.AppSettings.GetValues("SMTPPort")?.FirstOrDefault();
+
+            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(strPort) || !int.TryParse(strPort, out int port))
+            {
+                _nextRunTime = DateTime.Now.AddSeconds(10);
+                return false;
+            }
+
+            try
+            {
+                Search<OutboundEmail> outboundEmailSearch = new Search<OutboundEmail>();
+                foreach (OutboundEmail email in outboundEmailSearch.GetEditableReader())
+                {
+                    SmtpClient smtpClient = new SmtpClient(server, port);
+                    MailMessage mailMessage = new MailMessage();
+                    MailAddress fromAddress = new MailAddress(email.FromEmail, email.FromName);
+                    mailMessage.From = fromAddress;
+                    mailMessage.To.Add(email.To);
+                    mailMessage.Subject = email.Subject;
+                    mailMessage.Body = email.Body;
+                    smtpClient.Send(mailMessage);
+
+                    email.Delete();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                _nextRunTime = DateTime.Now.AddSeconds(10);
+            }
+
+            return true;
+        }
+    }
+}

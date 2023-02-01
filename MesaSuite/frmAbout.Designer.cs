953cea0db22ff415cc79cb8e35383d24bb80806e
Chris <supersoftware1234@gmail.com>
2023-02-01 18:55:35 -0500

    Update frmAbout.Designer.cs

--- MesaSuite/frmAbout.Designer.cs
+++ MesaSuite/frmAbout.Designer.cs
@@ -93,6 +93,7 @@ namespace MesaSuite
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.HotTrack = true;
             this.tabControl1.ItemSize = new System.Drawing.Size(58, 18);
             this.tabControl1.Location = new System.Drawing.Point(12, 116);
@@ -142,9 +143,9 @@ namespace MesaSuite
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(5, 63);
+            this.label11.Location = new System.Drawing.Point(0, 63);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(66, 13);
+            this.label11.Size = new System.Drawing.Size(72, 13);
             this.label11.TabIndex = 4;
             this.label11.Text = "Our Website";
             this.label11.Click += new System.EventHandler(this.label11_Click);
@@ -176,7 +177,7 @@ namespace MesaSuite
             this.label12.AutoSize = true;
             this.label12.Location = new System.Drawing.Point(14, 63);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(46, 13);
+            this.label12.Size = new System.Drawing.Size(49, 13);
             this.label12.TabIndex = 4;
             this.label12.Text = "Dynmap";
             this.label12.Click += new System.EventHandler(this.label12_Click);
@@ -206,9 +207,9 @@ namespace MesaSuite
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(17, 64);
+            this.label10.Location = new System.Drawing.Point(14, 64);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(40, 13);
+            this.label10.Size = new System.Drawing.Size(44, 13);
             this.label10.TabIndex = 3;
             this.label10.Text = "GitHub";
             this.label10.Click += new System.EventHandler(this.label10_Click);
@@ -238,9 +239,9 @@ namespace MesaSuite
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox2.Image = global::MesaSuite.Properties.Resources.logo_cp;
-            this.pictureBox2.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox2.Location = new System.Drawing.Point(3, 18);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(311, 93);
+            this.pictureBox2.Size = new System.Drawing.Size(311, 91);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
@@ -274,9 +275,9 @@ namespace MesaSuite
             // linkExtraCredit
             // 
             this.linkExtraCredit.AutoSize = true;
-            this.linkExtraCredit.Location = new System.Drawing.Point(131, 315);
+            this.linkExtraCredit.Location = new System.Drawing.Point(125, 315);
             this.linkExtraCredit.Name = "linkExtraCredit";
-            this.linkExtraCredit.Size = new System.Drawing.Size(88, 13);
+            this.linkExtraCredit.Size = new System.Drawing.Size(100, 13);
             this.linkExtraCredit.TabIndex = 8;
             this.linkExtraCredit.TabStop = true;
             this.linkExtraCredit.Text = "Additional Credits";
@@ -301,7 +302,7 @@ namespace MesaSuite
             this.panel4.Controls.Add(this.label7);
             this.panel4.Controls.Add(this.label8);
             this.panel4.Controls.Add(this.pboxSloosecannon);
-            this.panel4.Location = new System.Drawing.Point(124, 176);
+            this.panel4.Location = new System.Drawing.Point(178, 176);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(102, 124);
             this.panel4.TabIndex = 5;
@@ -309,26 +310,26 @@ namespace MesaSuite
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(14, 83);
+            this.label9.Location = new System.Drawing.Point(11, 83);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.Size = new System.Drawing.Size(79, 13);
             this.label9.TabIndex = 1;
             this.label9.Text = "sloosecannon";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(19, 66);
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(20, 66);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.Size = new System.Drawing.Size(60, 13);
             this.label7.TabIndex = 3;
             this.label7.Text = "Developer";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(31, 100);
+            this.label8.Location = new System.Drawing.Point(28, 100);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 13);
             this.label8.TabIndex = 2;
@@ -354,7 +355,7 @@ namespace MesaSuite
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label6);
             this.panel3.Controls.Add(this.pboxRavenholmZombie);
-            this.panel3.Location = new System.Drawing.Point(178, 46);
+            this.panel3.Location = new System.Drawing.Point(70, 176);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(102, 124);
             this.panel3.TabIndex = 4;
@@ -362,28 +363,28 @@ namespace MesaSuite
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 66);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "UX Designer";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 100);
+            this.label5.Location = new System.Drawing.Point(17, 100);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.Size = new System.Drawing.Size(68, 13);
             this.label5.TabIndex = 2;
             this.label5.Text = "Christopher";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 83);
+            this.label6.Location = new System.Drawing.Point(-1, 83);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.Size = new System.Drawing.Size(102, 13);
             this.label6.TabIndex = 1;
             this.label6.Text = "RavenholmZombie";
             // 
@@ -409,34 +410,34 @@ namespace MesaSuite
             this.panel2.Controls.Add(this.pboxCSX8600);
             this.panel2.Location = new System.Drawing.Point(70, 46);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(102, 124);
+            this.panel2.Size = new System.Drawing.Size(210, 124);
             this.panel2.TabIndex = 0;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1, 66);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(64, 13);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.Size = new System.Drawing.Size(90, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Head Developer";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 100);
+            this.label2.Location = new System.Drawing.Point(64, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Adam";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 83);
+            this.label1.Location = new System.Drawing.Point(64, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "CSX8600";
             // 
@@ -444,11 +445,11 @@ namespace MesaSuite
             // 
             this.pboxCSX8600.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pboxCSX8600.ErrorImage = global::MesaSuite.Properties.Resources.failed;
-            this.pboxCSX8600.ImageLocation = "https://mc-heads.net/head/CSX8600";
+            this.pboxCSX8600.ImageLocation = "https://mc-heads.net/body/CSX8600";
             this.pboxCSX8600.InitialImage = global::MesaSuite.Properties.Resources.loading;
-            this.pboxCSX8600.Location = new System.Drawing.Point(25, 13);
+            this.pboxCSX8600.Location = new System.Drawing.Point(15, 13);
             this.pboxCSX8600.Name = "pboxCSX8600";
-            this.pboxCSX8600.Size = new System.Drawing.Size(50, 50);
+            this.pboxCSX8600.Size = new System.Drawing.Size(43, 93);
             this.pboxCSX8600.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pboxCSX8600.TabIndex = 0;
             this.pboxCSX8600.TabStop = false;

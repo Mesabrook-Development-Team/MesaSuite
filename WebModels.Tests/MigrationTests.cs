cb3613a808bc84732b27be4112859a13fd2e7bc7
Adam Clussman <cnwaj@hotmail.com>
2023-06-15 14:49:44 -0500

    Fix unit test?

--- WebModels.Tests/MigrationTests.cs
+++ WebModels.Tests/MigrationTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebModels.Migrations;
 
@@ -15,13 +16,20 @@ namespace WebModels.Tests
             Type migrationType = typeof(IMigration);
             HashSet<int> migrationsUsed = new HashSet<int>();
             HashSet<int> duplicateMigrations = new HashSet<int>();
-            foreach(Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(t => t != migrationType && migrationType.IsAssignableFrom(t))))
+            foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                IMigration migration = (IMigration)Activator.CreateInstance(type);
-                if (!migrationsUsed.Add(migration.MigrationNumber))
+                try
                 {
-                    duplicateMigrations.Add(migration.MigrationNumber);
+                    foreach (Type type in assembly.GetTypes().Where(t => t != migrationType && migrationType.IsAssignableFrom(t)))
+                    {
+                        IMigration migration = (IMigration)Activator.CreateInstance(type);
+                        if (!migrationsUsed.Add(migration.MigrationNumber))
+                        {
+                            duplicateMigrations.Add(migration.MigrationNumber);
+                        }
+                    }
                 }
+                catch { }
             }
 
             Assert.AreEqual(0, duplicateMigrations.Count, "There are duplicate migration numbers: " + String.Join(", ", duplicateMigrations));

9b32e664b9eeb3bf6fde4fc5fea506e0aee38cbc
Adam Clussman <adamc@skyward.com>
2023-07-12 14:56:13 -0500

    Law data objects

---
+++ WebModels/gov/Law.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClussPro.ObjectBasedFramework;
+using ClussPro.ObjectBasedFramework.Schema.Attributes;
+using ClussPro.ObjectBasedFramework.Validation.Attributes;
+
+namespace WebModels.gov
+{
+    [Table("784106E7-D8C8-44C5-84A4-67834F266F7D")]
+    public class Law : DataObject
+    {
+        protected Law() : base() { }
+
+        private long? _lawID;
+        [Field("1724FBA2-A854-4C8F-98A7-32EC2B3C35D9")]
+        public long? LawID
+        {
+            get { CheckGet(); return _lawID; }
+            set { CheckSet(); _lawID = value; }
+        }
+
+        private long? _governmentID;
+        [Field("DE02428E-C471-4F4C-8F69-50DA35CEDBF8")]
+        [Required]
+        public long? GovernmentID
+        {
+            get { CheckGet(); return _governmentID; }
+            set { CheckSet(); _governmentID = value; }
+        }
+
+        private Government _government = null;
+        [Relationship("1BA7D11D-F8DB-452B-9789-68053889D4E4")]
+        public Government Government
+        {
+            get { CheckGet(); return _government; }
+        }
+
+        private string _name;
+        [Field("2F44ED9E-487B-4D98-B904-F039C601D5F2", DataSize = 30)]
+        [Required]
+        public string Name
+        {
+            get { CheckGet(); return _name; }
+            set { CheckSet(); _name = value;}
+        }
+
+        #region Relationships
+        #region gov
+        private List<LawSection> _lawSections = new List<LawSection>();
+        [RelationshipList("7D52FB1A-434E-404A-BCEA-FE93EABB96E4", nameof(LawSection.LawID))]
+        public IReadOnlyCollection<LawSection> LawSections
+        {
+            get { CheckGet(); return _lawSections; }
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ WebModels/gov/LawSection.cs
@@ -0,0 +1,51 @@
+﻿using ClussPro.ObjectBasedFramework;
+using ClussPro.ObjectBasedFramework.Schema;
+using ClussPro.ObjectBasedFramework.Schema.Attributes;
+
+namespace WebModels.gov
+{
+    [Table("4F63CD0E-BA2D-4C64-B0C1-F3EC354E2CFE")]
+    public class LawSection : DataObject
+    {
+        protected LawSection() : base() { }
+
+        private long? _lawSectionID;
+        [Field("CD46FED7-C13B-424B-8F7F-39E958E4C64E")]
+        public long? LawSectionID
+        {
+            get { CheckGet(); return _lawSectionID; }
+            set { CheckSet(); _lawSectionID = value; }
+        }
+
+        private long? _lawID;
+        [Field("8BF34F8F-7290-4529-A381-552025DCBE4B")]
+        public long? LawID
+        {
+            get { CheckGet(); return _lawID; }
+            set { CheckSet(); _lawID = value; }
+        }
+
+        private Law _law = null;
+        [Relationship("8A2A7239-F1D4-42D1-B848-87AC427DBEA7")]
+        public Law Law
+        {
+            get { CheckGet(); return _law; }
+        }
+
+        private string _title;
+        [Field("4E1A6F80-E847-4DDF-B669-3E8470D5E3C8", DataSize = 30)]
+        public string Title
+        {
+            get { CheckGet(); return _title; }
+            set { CheckSet(); _title = value;}
+        }
+
+        private string _detail;
+        [Field("CE5AF076-3AE7-4B93-830E-4AE787666946", DataSize = -1)]
+        public string Detail
+        {
+            get { CheckGet(); return _detail; }
+            set { CheckSet(); _detail = value; }
+        }
+    }
+}

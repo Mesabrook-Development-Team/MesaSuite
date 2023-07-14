488d6db6e57c358a0b3f5cea073c7b16f206999c
Chris <supersoftware1234@gmail.com>
2023-07-14 15:56:01 -0400

    Added MCSync step to Installer for Adam to wire up

---
+++ Updater/Steps/SetMCSyncPathStepControl.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Updater.Steps
+{
+    partial class SetMCSyncPathStepControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMinecraftDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Set up MCSync";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(359, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MCSync is Mesabrook\'s in-house designed Modpack downloader program.\r\n\r\nHere you c" +
+    "an set the location of your .minecraft folder for MCSync to use.";
+            // 
+            // txtMinecraftDirectory
+            // 
+            this.txtMinecraftDirectory.Location = new System.Drawing.Point(22, 165);
+            this.txtMinecraftDirectory.Name = "txtMinecraftDirectory";
+            this.txtMinecraftDirectory.Size = new System.Drawing.Size(404, 20);
+            this.txtMinecraftDirectory.TabIndex = 7;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(432, 165);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Specify your .minecraft folder:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(413, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "You can change this later on through MCSync by launching it from the Dashboard an" +
+    "d\r\nselecting Options > Minecraft Directory.";
+            // 
+            // SetMCSyncPathStepControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtMinecraftDirectory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "SetMCSyncPathStepControl";
+            this.Size = new System.Drawing.Size(538, 318);
+            this.Load += new System.EventHandler(this.SetMCSyncPathStepControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinecraftDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Updater/Steps/SetMCSyncPathStepControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updater.Steps
+{
+    public partial class SetMCSyncPathStepControl : UserControl
+    {
+        public SetMCSyncPathStepControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SetMCSyncPathStepControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

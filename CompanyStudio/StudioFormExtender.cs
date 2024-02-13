467dadc454a476b46ab743432213517e6b7cf0fe
Adam Clussman <cnwaj@hotmail.com>
2024-02-13 10:28:06 -0600

    Fix #170

--- CompanyStudio/StudioFormExtender.cs
+++ CompanyStudio/StudioFormExtender.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
@@ -22,80 +23,90 @@ namespace CompanyStudio
 
         public void ApplyStyle(Control control, ThemeBase theme)
         {
-            control.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-            control.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-
-            foreach(Button button in control.Controls.OfType<Button>())
+            try
             {
-                button.ForeColor = Color.Black;
-            }
+                control.ControlAdded += (s, e) => ApplyStyle(e.Control, theme);
 
-            foreach(Loader loader in control.Controls.OfType<Loader>())
-            {
-                loader.BackColor = Color.Transparent;
-            }
+                control.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                control.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
 
-            foreach(ListView listView in control.Controls.OfType<ListView>())
-            {
-                listView.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-                listView.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-            }
-
-            foreach (TreeView treeView in control.Controls.OfType<TreeView>())
-            {
-                treeView.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-                treeView.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-            }
-
-            foreach (ListBox listBox in control.Controls.OfType<ListBox>())
-            {
-                listBox.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-                listBox.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-            }
-
-            foreach(TabControl tabControl in control.Controls.OfType<TabControl>())
-            {
-                ApplyStyle(tabControl, theme);
-                tabControl.ControlAdded += (tabCtrl, e) => ApplyStyle(e.Control, theme);
-                foreach (TabPage page in tabControl.TabPages)
+                foreach (Button button in control.Controls.OfType<Button>())
                 {
-                    ApplyStyle(page, theme);
+                    button.ForeColor = Color.Black;
+                }
+
+                foreach (Loader loader in control.Controls.OfType<Loader>())
+                {
+                    loader.BackColor = Color.Transparent;
+                }
+
+                foreach (ListView listView in control.Controls.OfType<ListView>())
+                {
+                    listView.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                    listView.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
+                }
+
+                foreach (TreeView treeView in control.Controls.OfType<TreeView>())
+                {
+                    treeView.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                    treeView.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
+                }
+
+                foreach (ListBox listBox in control.Controls.OfType<ListBox>())
+                {
+                    listBox.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                    listBox.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
+                }
+
+                foreach (TabControl tabControl in control.Controls.OfType<TabControl>())
+                {
+                    ApplyStyle(tabControl, theme);
+                    tabControl.ControlAdded += (tabCtrl, e) => ApplyStyle(e.Control, theme);
+                    foreach (TabPage page in tabControl.TabPages)
+                    {
+                        ApplyStyle(page, theme);
+                    }
+                }
+
+                foreach (DataGridView dataGridView in control.Controls.OfType<DataGridView>())
+                {
+                    dataGridView.BackgroundColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                    dataGridView.ForeColor = Color.Black;
+                }
+
+                foreach (ToolStrip toolStrip in control.Controls.OfType<ToolStrip>())
+                {
+                    toolStrip.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+                    toolStrip.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
+                }
+
+                foreach (Panel panel in control.Controls.OfType<Panel>())
+                {
+                    ApplyStyle(panel, theme);
+                }
+
+                foreach (UserControl userControl in control.Controls.OfType<UserControl>())
+                {
+                    ApplyStyle(userControl, theme);
+                }
+
+                foreach (GroupBox groupBox in control.Controls.OfType<GroupBox>())
+                {
+                    //groupBox.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
+                    //groupBox.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
+
+                    ApplyStyle(groupBox, theme);
+                }
+
+                foreach (SplitContainer splitContainer in control.Controls.OfType<SplitContainer>())
+                {
+                    ApplyStyle(splitContainer, theme);
                 }
             }
-
-            foreach(DataGridView dataGridView in control.Controls.OfType<DataGridView>())
+            catch(Exception ex)
             {
-                dataGridView.BackgroundColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-                dataGridView.ForeColor = Color.Black;
-            }
-
-            foreach(ToolStrip toolStrip in control.Controls.OfType<ToolStrip>())
-            {
-                toolStrip.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-                toolStrip.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-            }
-
-            foreach(Panel panel in control.Controls.OfType<Panel>())
-            {
-                ApplyStyle(panel, theme);
-            }
-
-            foreach(UserControl userControl in control.Controls.OfType<UserControl>())
-            {
-                ApplyStyle(userControl, theme);
-            }
-
-            foreach(GroupBox groupBox in control.Controls.OfType<GroupBox>())
-            {
-                //groupBox.ForeColor = theme.ColorPalette.CommandBarMenuDefault.Text;
-                //groupBox.BackColor = theme.ColorPalette.CommandBarToolbarDefault.Background;
-
-                ApplyStyle(groupBox, theme);
-            }
-
-            foreach(SplitContainer splitContainer in control.Controls.OfType<SplitContainer>())
-            {
-                ApplyStyle(splitContainer, theme);
+                // There's no great way to log this. It's also not super important.
+                // The user will likely let us know if something is displayed wrong.
             }
         }
     }
